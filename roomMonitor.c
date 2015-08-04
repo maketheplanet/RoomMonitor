d270bf63e07058706f050c64213d21ede219e99c
makeThePlanet <heathcasey@maketheplanet.com>
2015-08-04 02:27:49 +0000

    added a loop to make sure sensor gets a good read

--- roomMonitor.c
+++ roomMonitor.c
@@ -2,11 +2,22 @@
 #include "dht11Driver/bbb_dht_read.h"
 
 int main(int argc, char *argv[]){
-	int sensor = 11, base = 1,number = 13;
-	float humidity = 0, temperature=0;
 	// pin P8_11 is base 1, gpio 13
-	int result = bbb_dht_read(sensor,base,number,&humidity,&temperature);
-
-	printf("The temp is %4.2f and the Humidity is %4.2f",humidity,temperature);
+	int sensor = 11, base = 1,number = 13;
+	float humidity = 0, temperature=0,tempF=0 ;
+	int timeout=0;
+	while(humidity==0){
+		int result = bbb_dht_read(sensor,base,number,&humidity,&temperature);
+		++timeout;
+		if(timeout > 10)
+		{
+			break;
+		}
+	
+	}
+	if(temperature > 0){
+		tempF = (temperature*9)/5+32;
+	}
+	printf("The Humidity is %4.2f and the Temperature is %4.2f\n",humidity,tempF);
 	return 0;
 }
